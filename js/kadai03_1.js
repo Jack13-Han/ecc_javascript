e5f0d4dd3ac89ba299ff4254a7b78685de9394bf
Jack13-Han <hanwaihtun1e.com>
2025-10-06 16:41:16 +0900

    new add kadai 3

---
+++ js/kadai03_1.js
@@ -0,0 +1,15 @@
+const title =document.getElementById("title")
+
+const message=document.getElementById("message")
+
+
+console.log(title)
+console.log(message)
+
+///dir
+console.dir(title)
+console.dir(message)
+
+console.log(title.innerHTML)
+console.log(message.innerHTML)
+
---
+++ js/test.js
@@ -0,0 +1,20 @@
+const aa = document.querySelector("#aa");
+const bb = document.querySelector("#bb");
+const cc = document.querySelector("#cc");
+const dd = document.querySelector("#dd");
+
+aa.addEventListener("click", () => {
+  console.log("u click aa");
+},true);
+
+bb.addEventListener("click", () => {
+  console.log("u click bb");
+},true);
+
+cc.addEventListener("click", () => {
+  console.log("u click cc");
+},true);
+
+dd.addEventListener("click", () => {
+  console.log("u click dd");
+},true);
