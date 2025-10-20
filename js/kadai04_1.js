7bf3823e6d0a92ab554403c203c82950a67bc0f0
Jack13-Han <hanwaihtun1e.com>
2025-10-20 14:35:14 +0900

    kadai 04_1 done

---
+++ js/kadai04_1.js
@@ -0,0 +1,32 @@
+// event handler
+const inputName = document.querySelector("#userName");
+const textName = document.querySelector("#userComment");
+const form = document.querySelector("#feedbackForm");
+
+//result output
+const result = document.querySelector("#result");
+
+form.addEventListener("submit",(e) => {
+    console.log(e.target)
+})
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+
+
+//   console.log(inputName.value);
+//   console.log(textName.value);
+//   console.log("submit");
+
+
+  result.textContent =` 
+    <h1>お名前 :  ${inputName.value} </h1>
+      <h4>ご意見 :  ${textName.value}</h4>
+  
+  ` ;
+
+  inputName.value = "";
+  textName.value = "";
+});
+
